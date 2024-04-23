bea1c58726e380e8913c3742e501ac97762e0710
Maxim <Sedov-mv@mail.ru>
2024-04-23 13:38:14 +0500

    Добавляет выполнение task_1

---
+++ task_1/constants.js
@@ -0,0 +1,6 @@
+//Суффикс полей для расшифолвки
+const desiredSuffix = 'Id';
+//Список полей исключений:
+const exclusionKeys = ['groupId', 'service', 'formatSize', 'ca']
+
+export { desiredSuffix, exclusionKeys }
--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,48 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from './data.js';
+import { desiredSuffix, exclusionKeys } from './constants.js'
 
 console.log("Let's rock")
-console.log(encoded, translations)
+// console.log(encoded, translations)
+
+const decoded = [];
+const decodedObject = {};
+const listIds = [];
+
+const getDecryptionField = (field, suffix) => field.endsWith(suffix);
+
+const getDecryptedValue = (encryptedValue) => translations[encryptedValue];
+
+const getDecoded = (exclusions) => {
+	decoded.length = 0;
+	encoded.forEach((group) => {
+		for (const key in group) {
+			if (!exclusions.includes(key)
+				&& getDecryptionField(key, desiredSuffix)
+				&& group[key] in translations) {
+				decodedObject[key] = getDecryptedValue(group[key]);
+			} else {
+				decodedObject[key] = group[key];
+				if (getDecryptionField(key, desiredSuffix) && !(group[key] in translations)) {
+					listIds.push(group[key])
+				};
+			}
+		}
+		decoded.push(decodedObject);
+	});
+};
 
 
 
-// console.log(decoded)
+getDecoded(exclusionKeys);
+console.log(listIds)
+const uniqueIds = listIds.reduce((acc, currentValue) => {
+	if (!acc.includes(currentValue)) {
+		acc.push(currentValue);
+	} else {
+		const index = acc.indexOf(currentValue)
+		acc.splice(index, 1)
+}
+	return acc
+}, []);
+console.log('Cписок уникальных id:', uniqueIds);
+console.log('Cписок объектов с расшифрованными полями с суффиксом id', decoded);
